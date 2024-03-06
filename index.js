e171ea287d25ef8dc19f9d286c24e66d45b6f573
Marine Dunstetter <bluecut.official@gmail.com>
2024-03-06 16:32:52 +0100

    Add complexity to the examples

--- index.js
+++ index.js
@@ -3,19 +3,29 @@
 module.exports = {
   name: require('./package').name,
 
-  contentFor: function(type) {
-    if (![
-      'head',
-      'test-head',
-      'head-footer',
-      'test-head-footer',
-      'body',
-      'test-body',
-      'body-footer',
-      'test-body-footer'
-    ].includes(type)) {
-      return '';
+  contentFor: function (type) {
+    switch (type) {
+      case 'head':
+      case 'test-head':
+      case 'head-footer':
+      case 'test-head-footer':
+        return `<script>console.log('The content-for "${type}" has been replaced by this console.log')</script>`;
+      case 'body':
+        return '<p>Have a look at the debugger console 👀</p>';
+      case 'body-footer':
+        return '<p>Type "view-source:" before the current URL to see how the HTML looks like</p>';
+      case 'test-body':
+      case 'test-body-footer':
+        return `<p>This snippet replaces content-for "${type}" in the app index.html</p>`;
+      case 'fox':
+        return `
+          <script>console.log(\`
+This is a multiline log.
+The string is built using back quotes.
+\`);
+          </script>`;
+      default:
+        return '';
     }
-    return `<p>This snippet replaces content-for "${type}" in the app index.html</p>`;
   },
 };
